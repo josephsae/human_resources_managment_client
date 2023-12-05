7d8ce105cc0dcde0a3a28ec53b577ed6b88ae27a
Joseph Brayan Smith Caicedo Sáenz <joseph.caice08@gmail.com>
2023-12-04 23:24:59 -0500

    docs: add javadoc

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.poli.human_resources_managment_client.client"},{"l":"com.poli.human_resources_managment_client.client.handlers"}];updateSearchResults();
